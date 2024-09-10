c25c7872ff0f173ac1a5d2acdfe89f973873662e
Daniel Bennett <danielpsalmbennett@yahoo.com>
2024-09-09 21:05:49 -0500

    chaning login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,16 +22,3 @@ export const NavBar = () => {
                                 localStorage.removeItem("rock_token")
                                 navigate('/login')
                             }}
-                        >Logout</button>
-                    </li> :
-                    <>
-                        <li className="navbar__item">
-                            <NavLink className="text-left underline text-blue-600 hover:text-purple-700" to={"/login"}>Login</NavLink>
-                        </li>
-                        <li className="navbar__item">
-                            <NavLink className="text-left underline text-blue-600 hover:text-purple-700" to={"/register"}>Register</NavLink>
-                        </li>
-                    </>
-            }        </ul>
-    )
-}
